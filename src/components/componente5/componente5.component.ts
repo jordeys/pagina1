21e5ae4de9a26ac774fe1aee6902e0e1e682d17b
mi primer commit <jordeysebas1@gmail.com>
2022-11-19 18:16:54 -0500

    primer commit

---
+++ src/components/componente5/componente5.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente5',
+  templateUrl: './componente5.component.html',
+  styleUrls: ['./componente5.component.css']
+})
+export class Componente5Component {
+
+}
---
+++ src/components/componente6/componente6.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente6',
+  templateUrl: './componente6.component.html',
+  styleUrls: ['./componente6.component.css']
+})
+export class Componente6Component {
+
+}
